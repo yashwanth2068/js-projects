c31d72741cf0d0b23e4c45a9954abcb31cd5bc16
yashwanth <mogantiyashwanth@gmail.com>
2025-06-02 15:22:12 +0530

    products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,4 @@
+/*
 const products =[{
   image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
   name:'  Black and Gray Athletic Cotton Socks - 6 Pairs',
@@ -22,8 +23,16 @@ const products =[{
     count: 56
   },
   priceCents: 799
+}, {
+  image:'images/products/black-2-slot-toaster.jpg',
+  name:'2 Slot Toaster - Black',
+  rating: {
+    stars: 5,
+    count: 2197
+  },
+  priceCents: 1899
 }];
-
+*/
 let productsHTML = '';
 
 products.forEach((product) => {
