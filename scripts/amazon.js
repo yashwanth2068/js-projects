82e8dc047d25480cdce06598802074534eefd1f0
yashwanth <mogantiyashwanth@gmail.com>
2025-06-03 10:37:05 +0530

    making the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -116,7 +116,13 @@ products.forEach((product) => {
     });
     }
 
+      let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
     
-    console.log(cart);
   });
  });
