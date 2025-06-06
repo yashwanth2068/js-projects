d7528895eda143cb0d96455fa575c483d30bfdb9
yashwanth <mogantiyashwanth@gmail.com>
2025-06-06 15:50:37 +0530

    deletes product from cart when clicking delete

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -37,6 +37,7 @@ import {cart,addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
+
   let productsHTML = '';
 
 products.forEach((product) => {
